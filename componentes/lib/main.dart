7696f706af4c1aafc6dbfc2eeed3495dec987a92
Milton Vargas <hadez2505@gmail.com>
2022-05-24 07:41:24 -0500

    HomeScreen Rutas iniciales

--- componentes/lib/main.dart
+++ componentes/lib/main.dart
@@ -7,10 +7,18 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: ListView2Screen(),
+      //home: ListView2Screen(),
+      initialRoute: '/home',
+      routes: {
+        '/alert'    : (context) => const AlertScreen(),
+        '/card'     : (context) => const CardScreen(),
+        '/home'     : (context) => const HomeScreen(),
+        '/listView1': (context) => const ListView1Screen(),
+        '/listView2': (context) => const ListView2Screen(),
+      },
     );
   }
 }
--- componentes/lib/screens/screens.dart
+++ componentes/lib/screens/screens.dart
@@ -1,3 +1,8 @@
 
+
+
+export 'package:componentes/screens/alert_screen.dart';
+export 'package:componentes/screens/card_screen.dart';
+export 'package:componentes/screens/home_screen.dart';
 export 'package:componentes/screens/listview1_screen.dart';
 export 'package:componentes/screens/listview2_screen.dart';
